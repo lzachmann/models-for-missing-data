7ce267c11e44436bad465d749d2e764256235e1d
Luke Zachmann <lzachmann@gmail.com>
2021-08-23 13:47:02 -0600

    refine wrap-r and add workflow dependency

--- .github/actions/call-r/src/wrap-r.sh
+++ .github/actions/call-r/src/wrap-r.sh
@@ -2,6 +2,19 @@
 
 # Action code
 
-Rscript "${1}" "${2}"
+R_FILE="${1:-model-api/.empty.R}"
+ARGS="${2:-none}"
+OPTS="${3:-none}"
+
+if [ "${ARGS}" == "none" && "${OPTS}" == "none" ]; then
+  Rscript "${R_FILE}"
+else
+  if [ "${ARGS}" != "none" && "${OPTS}" == "none" ]; then
+    Rscript "${R_FILE}" "${ARGS}"
+  else
+    Rscript "${R_FILE}" "${ARGS}" "${OPTS}"
+	fi
+fi
+
 time=$(date)
 echo "::set-output name=time::$time"
