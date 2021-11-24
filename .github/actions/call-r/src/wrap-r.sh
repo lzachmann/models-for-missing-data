c7a4142130f83817aefdeffa98a0ac369490f90d
Luke Zachmann <lzachmann@gmail.com>
2021-11-24 10:32:55 -0700

    model api breadcrumbs for build call and added missing brackets in wrap-r action

--- .github/actions/call-r/src/wrap-r.sh
+++ .github/actions/call-r/src/wrap-r.sh
@@ -6,10 +6,10 @@ R_FILE="${1:-model-api/.empty.R}"
 ARGS="${2:-none}"
 OPTS="${3:-none}"
 
-if [ "${ARGS}" == "none" && "${OPTS}" == "none" ]; then
+if [[ "${ARGS}" == "none" && "${OPTS}" == "none" ]]; then
   Rscript "${R_FILE}"
 else
-  if [ "${ARGS}" != "none" && "${OPTS}" == "none" ]; then
+  if [[ "${ARGS}" != "none" && "${OPTS}" == "none" ]]; then
     Rscript "${R_FILE}" "${ARGS}"
   else
     Rscript "${R_FILE}" "${ARGS}" "${OPTS}"
