5f45a7f5076add4331cf075d3dc41ed22160d441
Luke Zachmann <lzachmann@gmail.com>
2021-11-30 14:38:04 -0700

    using opts array in call-r action, added multidplyr to image

--- .github/actions/call-r/src/wrap-r.sh
+++ .github/actions/call-r/src/wrap-r.sh
@@ -6,17 +6,17 @@ R_FILE="${1:-model-api/.empty.R}"
 ARGS="${2:-none}"
 OPTS="${3:-none}"
 
+OPTS_ARRAY=($OPTS)
+
 if [[ "${ARGS}" == "none" && "${OPTS}" == "none" ]]; then
   Rscript "${R_FILE}"
 else
   if [[ "${ARGS}" != "none" && "${OPTS}" == "none" ]]; then
     Rscript "${R_FILE}" "${ARGS}"
   else
-    Rscript "${R_FILE}" "${ARGS}" "${OPTS}"
+    Rscript "${R_FILE}" "${ARGS}" "${OPTS_ARRAY[@]}"
 	fi
 fi
 
 EXIT_STATUS=$?
 echo "::set-output name=code::$EXIT_STATUS"
-# time=$(date)
-# echo "::set-output name=time::$time"
